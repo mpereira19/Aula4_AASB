36a4cc8942f0c6e55fc297fbfb34b6c4134408f4
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2020-11-10 16:06:52 +0000

    Update Ficha4.py

--- Ficha4.py
+++ Ficha4.py
@@ -1,1 +1,11 @@
-print('Hello world!')
+def complemento_inverso(seq):
+    
+    ''' 
+    Definição que introduzindo uma sequência devolve o seu complemento inverso
+    
+    Var:
+        seq: int
+    '''
+    
+    inverso = seq[::-1]
+    
