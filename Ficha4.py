f35087d5d1b5c60ba273513a8e8c2d22ef3a2289
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2020-11-10 16:23:06 +0000

    Update Ficha4.py
    Função do complemento inverso  de uma sequência com a função troca de apoio à função complemento_inverso.

--- Ficha4.py
+++ Ficha4.py
@@ -1,11 +1,32 @@
-def complemento_inverso(seq):
-    
-    ''' 
-    Definição que introduzindo uma sequência devolve o seu complemento inverso
-    
-    Var:
-        seq: int
+def troca (seq):
     '''
+    Definição que introduzindo uma sequência devolve essa sequência com as bases trocadas pelo seu par.
+
+    Parameters
+    ----------
+    seq : str
+
+    Returns
+    -------
+    comp.
+    '''
+    pares_bases = {'A':'T', 'C':'G', 'T':'A', 'G':'C'}
+    comp =''.join(pares_bases[n] for n in (seq))
+    return comp
+        
+
+def complemento_inverso(seq):
+    ''' 
+    Definição que introduzindo uma sequência devolve o seu complemento inverso.
+
+    Parameters
+    ----------    
+    seq: str
     
+    Returns
+    -------
+    comple_inv
+    '''
     inverso = seq[::-1]
-    
+    comple_inv=troca(inverso)
+    return comple_inv
