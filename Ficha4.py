036c7149c17c3d6c06ef2a3fe9190bde92b9e963
mpereira19 <74026152+mpereira19@users.noreply.github.com>
2020-11-10 17:32:51 +0000

    Update Ficha4.py
    Correção em tradução do return. organização do código, remoção de dicionário desnecessário e melhoramento da descrição das funções.

--- Ficha4.py
+++ Ficha4.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#=======
-=======
-<<<<<<< HEAD
-gencode = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
-
-=======
 def troca (seq):
     '''
     Definição que introduzindo uma sequência devolve essa sequência com as bases trocadas pelo seu par.
@@ -31,81 +8,49 @@ def troca (seq):
 
     Returns
     -------
-    comp.
+    comp
     '''
     pares_bases = {'A':'T', 'C':'G', 'T':'A', 'G':'C'}
     comp =''.join(pares_bases[n] for n in (seq))
     return comp
         
->>>>>>> faadbed02923efd01ba597da08773ee4ebc57f73
 
->>>>>>> 529ae2ea7479a1ba1beaafd1ebd11f9d18e2255a
 def complemento_inverso(seq):
     ''' 
-    Definição que introduzindo uma sequência devolve o seu complemento inverso.
+    Definição que introduzida uma sequência devolve o seu complemento inverso.
 
     Parameters
     ----------    
     seq: str
+    inverso : str
+    comple_inv : str
     
     Returns
     -------
     comple_inv
     '''
+    seq=seq.upper()
     inverso = seq[::-1]
-<<<<<<< HEAD
-    
-gencode = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}  
-
-def translation(seq):
-
-'''
-    Definição de uma função que traduza a sequencia de RNA numa sequencia de aminoácidos
-'''
-    seq = seq.upper()
-    for i in range(0, len(seq), 3):
-        codao = seq[i : i + 3] 
-        if i==0:
-            amino=gencode[codao]
-        elif len(codao)==3:
-            amino += gencode[codao]
-        else:
-            pass
-return amino
-
-=======
-<<<<<<< HEAD
     comple_inv=troca(inverso)
     return comple_inv
-=======
- 
-
-  
-'''
-    Definição que introduzida uma sequência de DNA devolve a sua sequência de RNA
     
-    Var:
-        seq: str
-        rna: str  
-'''
 def transcricao(seq):
-<<<<<<< HEAD
+    '''
+    Função que introduzida uma sequência de DNA devolve a sua sequência de RNA.
+
+    Parameters
+    ----------
+    seq : str
+    seq1 : str
+    rna : str
+    
+
+    Returns
+    -------
+    rna
+
+    '''
+    seq=seq.upper()
     rna=''
     seq1 = seq.rstrip()
     for i in seq1:
@@ -115,14 +60,47 @@ def transcricao(seq):
         else:
             rna += i
     return rna
- 
-=======
-        '''
+
     
+
+def traducao(seq):
+
+    '''
+    Função que introduzida uma sequência DNA devolve uma sequência de aminoácidos.
+
+    Parameters
+    ----------
+    seq : str
+    amino : str
+    gencode : dict
+    codao : str
+
+    Returns
+    -------
+    amino
+
+    '''
+    gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
     
-        '''
-        
-        
->>>>>>> 4eaecc565f676f8095790ffd207becd35dee78c4
->>>>>>> faadbed02923efd01ba597da08773ee4ebc57f73
->>>>>>> 529ae2ea7479a1ba1beaafd1ebd11f9d18e2255a
+    seq = seq.upper()
+    for i in range(0, len(seq), 3):
+        codao = seq[i : i + 3] 
+        if i==0:
+            amino=gencode[codao]
+        elif len(codao)==3:
+            amino += gencode[codao]
+        else:
+            pass
+    return amino
+
+  
+
+
