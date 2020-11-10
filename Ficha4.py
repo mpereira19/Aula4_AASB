05eedb441f6464625a1c255aa279c9a4be3a876e
julianapereira99 <juliana.pereira05.99@gmail.com>
2020-11-10 16:04:02 +0000

    Update Ficha4.py

--- Ficha4.py
+++ Ficha4.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 print('Hello World!')
 
-input('Quantos números precisas?')
-=======
-print('Hello world!')
->>>>>>> 5732b881ab099c13816497ac6e42c61d8d502b7d
+#input('Quantos números precisas?')
