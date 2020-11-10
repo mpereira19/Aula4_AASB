db9a39a7685b90e97441e203fe795b164d6c3e26
Joao16am <71942738+Joao16am@users.noreply.github.com>
2020-11-10 16:48:56 +0000

    Update Ficha4.py

--- Ficha4.py
+++ Ficha4.py
@@ -1,3 +1,22 @@
+gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+
 def complemento_inverso(seq):
     
     ''' 
@@ -9,12 +28,23 @@ def complemento_inverso(seq):
     
     inverso = seq[::-1]
  
+
+  
+'''
+    Definição que introduzida uma sequência de DNA devolve a sua sequência de RNA
     
- 
+    Var:
+        seq: str
+        rna: str  
+'''
 def transcricao(seq):
-        '''
-    
-    
-        '''
-        
-        
+    rna=''
+    seq1 = seq.rstrip()
+    for i in seq1:
+        if i=='T':
+            i='U'
+            rna += i
+        else:
+            rna += i
+    return rna
+ 
